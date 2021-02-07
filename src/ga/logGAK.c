23a943d3bb26af01fb0176141c1a887a7bc3219d
Haiko Thiessen <uxrdr@student.kit.edu>
2021-02-07 12:14:57 +0100

    formatting

--- src/ga/logGAK.c
+++ src/ga/logGAK.c
@@ -51,6 +51,7 @@
 
 #include <stdlib.h>
 #include <math.h>
+#include <logGAK.h>
 /* Useful constants */
 #define LOG0 -10000          /* log(0) */
 #define LOGP(x, y) (((x)>(y))?(x)+log1p(exp((y)-(x))):(y)+log1p(exp((x)-(y))))
--- src/ga/matrixlogGAK.c
+++ src/ga/matrixlogGAK.c
@@ -1,9 +1,10 @@
 
 #include <stdlib.h>
-#include <math.h>
-//#include "logGAK.c"
+#include <logGAK.h>
 #include <omp.h>
 #include <stdio.h>
+#include <matrixLogGAK.h>
+
 
 int g_nInstances;
 int g_nLength_train;
@@ -11,14 +12,14 @@ int g_nLength_test;
 int g_nDim;
 
 int resOffset(int i, int j) {
-    return i * g_nInstances + j; 
+    return i * g_nInstances + j;
 }
 
-int seqOffset_train(int i){
+int seqOffset_train(int i) {
     return i * g_nLength_train * g_nDim;
 }
 
-int seqOffset_test(int i){
+int seqOffset_test(int i) {
     return i * g_nLength_test * g_nDim;
 }
 
@@ -32,42 +33,41 @@ int seqOffset_test(int i){
 * sigma is the bandwidth parameter for the gauss-ish kernel
 * triangular is the parameter to trade off global alignment vs. optimal alignment, i.e., more diagonal alignment
 */
-void trainGramMatrixExp(double *seq, int nInstances, int nLength, int nDim, double *res, double sigma, int triangular)
-{
+void trainGramMatrixExp(double *seq, int nInstances, int nLength, int nDim, double *res, double sigma, int triangular) {
     g_nInstances = nInstances;
     g_nLength_train = nLength;
     g_nDim = nDim;
- 
-    int i = 0;
-    double *cache = (double*) malloc(nInstances * sizeof(double));
 
-    if(cache == NULL)
-    {
+    int i = 0;
+    double *cache = (double *) malloc(nInstances * sizeof(double));
+
+    if (cache == NULL) {
         printf("Error! memory not allocated.");
         exit(0);
     }
     // compute GAK with itself
-    #pragma omp parallel for private(i)
-    for(i = 0; i < nInstances; i++){
+#pragma omp parallel for private(i)
+    for (i = 0; i < nInstances; i++) {
         int seq_i = seqOffset_train(i);
-        cache[i] = logGAK((double*) &seq[seq_i], (double*) &seq[seq_i], nLength, nLength, nDim, sigma, triangular);
+        cache[i] = logGAK((double *) &seq[seq_i], (double *) &seq[seq_i], nLength, nLength, nDim, sigma, triangular);
     }
 
-    #pragma omp parallel for private(i)
-    for(i = 0; i < nInstances; i++){
+#pragma omp parallel for private(i)
+    for (i = 0; i < nInstances; i++) {
         int j = 0;
         int seq_i = seqOffset_train(i);
-        for(j = i; j < nInstances; j++){
+        for (j = i; j < nInstances; j++) {
             int seq_j = seqOffset_train(j);
-            
+
             // compute the global alignment kernel value
-            double ga12 = logGAK((double*) &seq[seq_i], (double*) &seq[seq_j], nLength, nLength, nDim, sigma, triangular);
+            double ga12 = logGAK((double *) &seq[seq_i], (double *) &seq[seq_j], nLength, nLength, nDim, sigma,
+                                 triangular);
             // compute the normalization factor
             //double ga11 = logGAK((double*) &seq[seq_i], (double*) &seq[seq_i], nLength, nLength, nDim, sigma, triangular);
             double ga11 = cache[i];
             //double ga22 = logGAK((double*) &seq[seq_j], (double*) &seq[seq_j], nLength, nLength, nDim, sigma, triangular);
             double ga22 = cache[j];
-            double nf = 0.5*(ga11+ga22);
+            double nf = 0.5 * (ga11 + ga22);
             double mlnk = nf - ga12;
             int res_ij = resOffset(i, j);
             int res_ji = resOffset(j, i);
@@ -76,54 +76,55 @@ void trainGramMatrixExp(double *seq, int nInstances, int nLength, int nDim, doub
         }
     }
     free(cache);
-    
-    
+
 
 }
 
-void testGramMatrixExp(double *train, double *test, int nInstances_train, int nInstances_test, int nLength_train, int nLength_test,int nDim, double *res, double sigma, int triangular)
-{
+void testGramMatrixExp(double *train, double *test, int nInstances_train, int nInstances_test, int nLength_train,
+                       int nLength_test, int nDim, double *res, double sigma, int triangular) {
     g_nInstances = nInstances_train;
     g_nLength_test = nLength_test;
     g_nLength_train = nLength_train;
-    g_nDim= nDim;
+    g_nDim = nDim;
 
     int i = 0;
     int j = 0;
-    double *cache_train = (double*) malloc(nInstances_train * sizeof(double));
-    double *cache_test= (double*) malloc(nInstances_test * sizeof(double));
+    double *cache_train = (double *) malloc(nInstances_train * sizeof(double));
+    double *cache_test = (double *) malloc(nInstances_test * sizeof(double));
 
-    if(cache_train == NULL || cache_test == NULL)
-    {
+    if (cache_train == NULL || cache_test == NULL) {
         printf("Error! memory not allocated.");
         exit(0);
     }
     // compute GAK with itself
-    #pragma omp parallel for private(i)
-    for(i = 0; i < nInstances_train; i++){
+#pragma omp parallel for private(i)
+    for (i = 0; i < nInstances_train; i++) {
         int seq_i = seqOffset_train(i);
-        cache_train[i] = logGAK((double*) &train[seq_i], (double*) &train[seq_i], nLength_train, nLength_train, nDim, sigma, triangular);
+        cache_train[i] = logGAK((double *) &train[seq_i], (double *) &train[seq_i], nLength_train, nLength_train, nDim,
+                                sigma, triangular);
     }
-    #pragma omp parallel for private(i)
-    for(i = 0; i < nInstances_test; i++){
+#pragma omp parallel for private(i)
+    for (i = 0; i < nInstances_test; i++) {
         int seq_i = seqOffset_test(i);
-        cache_test[i] = logGAK((double*) &test[seq_i], (double*) &test[seq_i], nLength_test, nLength_test, nDim, sigma, triangular);
+        cache_test[i] = logGAK((double *) &test[seq_i], (double *) &test[seq_i], nLength_test, nLength_test, nDim,
+                               sigma, triangular);
     }
 
-    #pragma omp parallel for collapse(2) private(i, j)
-    for(i = 0; i < nInstances_test; i++){
-        for(j = 0; j < nInstances_train; j++){
+#pragma omp parallel for collapse(2) private(i, j)
+    for (i = 0; i < nInstances_test; i++) {
+        for (j = 0; j < nInstances_train; j++) {
             int seq_test = seqOffset_test(i);
             int seq_train = seqOffset_train(j);
 
             // compute the global alignment kernel value
-            double ga12 = logGAK((double*) &train[seq_train], (double*) &test[seq_test], nLength_train, nLength_test, nDim, sigma, triangular);
+            double ga12 = logGAK((double *) &train[seq_train], (double *) &test[seq_test], nLength_train, nLength_test,
+                                 nDim, sigma, triangular);
             // compute the normalization factor
             //double ga11 = logGAK((double*) &train[seq_train], (double*) &train[seq_train], nLength_train, nLength_train, nDim, sigma, triangular);
             double ga11 = cache_train[j];
             //double ga22 = logGAK((double*) &test[seq_test], (double*) &test[seq_test], nLength_test, nLength_test, nDim, sigma, triangular);
             double ga22 = cache_test[i];
-            double nf = 0.5*(ga11+ga22);
+            double nf = 0.5 * (ga11 + ga22);
             double mlnk = nf - ga12;
             int res_k = resOffset(i, j);
             res[res_k] = mlnk;
