e730483ce9ccea39def06ca04a8a7442c5d6678d
Haiko Thiessen <uxrdr@student.kit.edu>
2021-02-15 11:37:31 +0100

    Some plots, will be transferred to jupyter notebooks.

--- main.py
+++ main.py
@@ -1,17 +1,80 @@
-from tsvdd.libsvm import svmutil
 import numpy as np
 from sklearn.datasets import make_circles
 import matplotlib.pyplot as plt
-from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics.pairwise import linear_kernel
 from sklearn.model_selection import train_test_split
 import numpy as np
-from tsvdd.ga import tga_dissimilarity, train_kernel_matrix, test_kernel_matrix
-from tsvdd.utils import svmlib_kernel_format
+# from tsvdd.ga import tga_dissimilarity, train_kernel_matrix, test_kernel_matrix
+#from tsvdd.utils import svmlib_kernel_format
+#from sklearn.svm import SVDD
+from tsvdd import libsvdd
 
+class DirtySVDD:
+    _kernels = ["linear", "poly", "rbf", "sigmoid", "precomputed"]
 
-n_samples = 2000
+    def __init__(self, kernel='linear', C=0.002, degree=3, gamma=1,
+                 coef0=0.0, tol=1e-3, nu=0.5, shrinking=False, cache_size=200,
+                 verbose=True):
+        if kernel not in self._kernels:
+            raise ValueError()
+        self.kernel = kernel
+        self.C = C
+        self.support_ = None
+        self.support_vectors_ = None
+        self._n_support = None
+        self.dual_coef_ = None
+        self.intercept_ = None
+        self._probA = None
+        self._probB = None
+        self.degree = degree
+        self.shrinking=shrinking
+        self.gamma = gamma
+        self.coef0 = coef0
+        self.tol = tol
+        self.nu = nu
+        self.cache_size = cache_size
+        self.verbose = verbose
+        self.class_weight_ = np.empty(0, dtype=np.float64)
+        self.probability = False
+        self.epsilon = 0
+        self.is_fit = False
+        self.n_SV = None
+
+    def fit(self, X):
+        y = np.ones(X.shape[0], dtype=np.float64)
+        sample_weight = np.ones(X.shape[0], dtype=np.float64)
+
+        self.support_, self.support_vectors_, self._n_support, \
+        self.dual_coef_, self.intercept_, self._probA, \
+        self._probB = libsvdd.fit(
+                X, y,
+                svm_type=5, sample_weight=sample_weight,
+                class_weight=self.class_weight_, kernel=self.kernel, C=self.C,
+                nu=self.nu, probability=self.probability, degree=self.degree,
+                shrinking=self.shrinking, tol=self.tol,
+                cache_size=self.cache_size, coef0=self.coef0,
+                gamma=self.gamma, epsilon=self.epsilon)
+        self.is_fit = True
+        self.n_SV = self.support_vectors_.shape[0]
+
+    def predict(self, X, K_xx_s=None):
+        assert self.is_fit
+        if K_xx_s is None and self.kernel == 'precomputed':
+            K_xx_s = np.array([10]*X.shape[0], dtype=np.float64)
+        else:
+            K_xx_s = np.ascontiguousarray(K_xx_s, dtype=np.float64)
+            if K_xx_s.ndim != 1:
+                raise ValueError('K_xx_s ndim unequal 1')
+        return libsvdd.predict(
+            X, K_xx_s, self.support_, self.support_vectors_, self._n_support,
+            self.dual_coef_, self.intercept_,
+            self._probA, self._probB, svm_type=5, kernel=self.kernel,
+            degree=self.degree, coef0=self.coef0, gamma=self.gamma,
+            cache_size=self.cache_size)
+
+n_samples = 1000
 outlier_ratio = 0.5
-X, y = make_circles(n_samples=n_samples, factor=.3, noise=0.01)
+X, y = make_circles(n_samples=n_samples, factor=.3, noise=0.05)
 y[y == 0] = -1
 
 
@@ -39,18 +102,47 @@ axs[1].set_title('Train split')
 axs[1].set_aspect(aspect='equal')
 plt.show()
 
-gram_train = rbf_kernel(X_train, X_train)
-gram_test = rbf_kernel(X_test, X_train)
-gram_diagonal_test = np.diagonal(rbf_kernel(X_test, X_test))
+gram_train = linear_kernel(X_train, X_train)
+gram_test = linear_kernel(X_test, X_train)
+gram_diagonal_test = np.diagonal(linear_kernel(X_test, X_test))
 gram_diagonal_train = np.diagonal(gram_train)
 
-X_libsvm_train = svmlib_kernel_format(gram_train)
-X_libsvm_test = svmlib_kernel_format(gram_test)
-prob = svmutil.svm_problem(np.ones(n_instances), X_libsvm_train, isKernel=True)
-param = svmutil.svm_parameter(f'-s 5 -t 4 -n {outlier_ratio_train}')
-model = svmutil.svm_train(prob, param)
-y_pred_test, p_val = svmutil.svm_predict(X_libsvm_test, gram_diagonal_test, model)
-y_pred_train, p_val = svmutil.svm_predict(X_libsvm_train, gram_diagonal_train, model)
+# PRECOMPUTED = False
+# if PRECOMPUTED:
+#     X_libsvm_train = svmlib_kernel_format(gram_train)
+#     X_libsvm_test = svmlib_kernel_format(gram_test)
+#     prob = svmutil.svm_problem(np.ones(n_instances), X_libsvm_train, isKernel=True)
+# else:
+#     X_libsvm_train = X_train.tolist()
+#     X_libsvm_test = X_test.tolist()
+#     prob = svmutil.svm_problem(np.ones(n_instances), X_libsvm_train, isKernel=False)
+
+C = 1 / (outlier_ratio_train * n_instances)
+if C <= 1/n_samples or C >= 1:
+    assert False
+# if PRECOMPUTED:
+#     param = svmutil.svm_parameter(f'-s 5 -t 4 -c {C} -e {10e-10} -h 0')
+# else:
+#     param = svmutil.svm_parameter(f'-s 5 -t 0 -c {C} -e {10e-10} -h 0')
+
+#svdd = SVDD(kernel='linear', nu=outlier_ratio_train, verbose=True, tol=10e-10, shrinking=False)
+#svdd.fit(X_train)
+
+svdd = DirtySVDD(kernel='precomputed', C=C,verbose=True, tol=10e-10, shrinking=False)
+svdd.fit(gram_train)
+print("gram_train")
+print(gram_train)
+print("gram_diagonal_train")
+print(gram_diagonal_train)
+print("dual_coef_")
+print(svdd.dual_coef_)
+print("support_")
+print(svdd.support_)
+#model = svmutil.svm_train(prob, param)
+# y_pred_test, p_val = svmutil.svm_predict(X_libsvm_test, gram_diagonal_test, model)
+# y_pred_train, p_val = svmutil.svm_predict(X_libsvm_train, gram_diagonal_train, model)
+y_pred_train = svdd.predict(gram_train, gram_diagonal_train)
+y_pred_test = svdd.predict(gram_test, gram_diagonal_test)
 y_pred_test = np.array(y_pred_test)
 y_pred_train = np.array(y_pred_train)
 
@@ -70,6 +162,9 @@ axs[1].scatter(X_train[blues, 0], X_train[blues, 1], c="blue", s=20, edgecolor='
 axs[1].set_title('Train split SVDD')
 axs[1].set_aspect(aspect='equal')
 plt.show()
+
+print(f"Inlier in y_pred_train: {y_pred_train[y_pred_train==1].shape[0]}")
+print(f"Inlier in y_train: {y_train[y_train==1].shape[0]}")
 
 1
 
--- main_al.py
+++ main_al.py
@@ -4,18 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.model_selection import train_test_split
 import numpy as np
-from tsvdd.utils import svmlib_kernel_format
 #from tsvdd import SVDD
-from sklearn.svm import SVDD
+from tsvdd.SVDD import SVDD
 from sklearn.metrics import balanced_accuracy_score
 
 
-def plot_train_test_pred(X_train, X_test, y_train, y_test, y_pred_train=None, y_pred_test=None):
+def plot_train_test_pred(X_train, X_test, y_train, y_test, y_pred_train=None, y_pred_test=None, r_square=None):
     if y_pred_test is not None and y_pred_train is not None:
         fig, axs = plt.subplots(2, 2, figsize=(10, 6))
     else:
         fig, axs = plt.subplots(1, 2, figsize=(12, 12), dpi=600)
-    fig.suptitle('Active learning Data with random cirlce', fontsize=12)
+    fig.suptitle('Active learning Data with cirlce', fontsize=12)
     reds = y_train == -1
     blues = y_train == 1
 
@@ -36,6 +35,9 @@ def plot_train_test_pred(X_train, X_test, y_train, y_test, y_pred_train=None, y_
         reds = y_pred_train == -1
         blues = y_pred_train == 1
 
+        if r_square:
+            circle = plt.Circle((0, 0), r_square, color='b', fill=False)
+            axs[1,0].add_patch(circle)
         axs[1,0].scatter(X_train[reds, 0], X_train[reds, 1], c="red", s=20, edgecolor='k')
         axs[1,0].scatter(X_train[blues, 0], X_train[blues, 1], c="blue", s=20, edgecolor='k')
         axs[1,0].set_title('Train split alSVDD')
@@ -44,9 +46,9 @@ def plot_train_test_pred(X_train, X_test, y_train, y_test, y_pred_train=None, y_
         reds = y_pred_test == -1
         blues = y_pred_test == 1
 
-        # if r_square:
-        #     circle = plt.Circle((0, 0), r_square, color='b', fill=False)
-        #     axs[1,1].add_patch(circle)
+        if r_square:
+            circle = plt.Circle((0, 0), r_square, color='b', fill=False)
+            axs[1,1].add_patch(circle)
         axs[1,1].scatter(X_test[reds, 0], X_test[reds, 1], c="red", s=20, edgecolor='k')
         axs[1,1].scatter(X_test[blues, 0], X_test[blues, 1], c="blue", s=20, edgecolor='k')
         axs[1,1].set_title('Test split alSVDD')
@@ -90,7 +92,7 @@ def decision_boundary_train_test(X_train, X_test, y_pred_train, y_pred_test, xx,
 
     plt.show()
 
-def decision_boundary(X_train, y_pred_train, xx, yy, Z, title=None):
+def decision_boundary(X, y, xx, yy, Z,title=None, sets=None):
     fig, axs = plt.subplots(1, 1, figsize=(10, 6))
     if title:
         fig.suptitle(f'{title}', fontsize=12)
@@ -104,20 +106,50 @@ def decision_boundary(X_train, y_pred_train, xx, yy, Z, title=None):
     axs.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred',
                    zorder=-97)
 
-    reds = y_pred_train == -1
-    blues = y_pred_train == 1
+    if sets is None:
+        reds = y == -1
+        blues = y == 1
 
-    axs.scatter(X_train[reds, 0], X_train[reds, 1], c="red", s=20, edgecolor='k')
-    axs.scatter(X_train[blues, 0], X_train[blues, 1], c="blue", s=20, edgecolor='k')
-    axs.set_title('Train split')
+        axs.scatter(X[reds, 0], X[reds, 1], c="red", s=20, edgecolor='k')
+        axs.scatter(X[blues, 0], X[blues, 1], c="blue", s=20, edgecolor='k')
+
+    else:
+        (U, L_in, L_out) = sets
+        # Unknown points
+        reds = y[U] == -1
+        blues = y[U] == 1
+
+        axs.scatter(X[U][reds, 0], X[U][reds, 1], c="red", s=20, edgecolor='k')
+        axs.scatter(X[U][blues, 0], X[U][blues, 1], c="blue", s=20, edgecolor='k')
+
+        # Inliers
+        reds = y[L_in] == -1
+        blues = y[L_in] == 1
+
+        if len(blues) > 0:
+            axs.scatter(X[L_in][reds, 0], X[L_in][reds, 1], c="gold", s=20, marker='*', edgecolor='k')
+        if len(reds) > 0:
+            axs.scatter(X[L_in][blues, 0], X[L_in][blues, 1], c="gold", s=20, marker='*', edgecolor='k')
+
+        # Outliers
+        reds = y[L_out] == -1
+        blues = y[L_out] == 1
+
+        if len(blues) > 0:
+            axs.scatter(X[L_out][reds, 0], X[L_out][reds, 1], c="gold", s=20, marker='*', edgecolor='k')
+        if len(reds) > 0:
+            axs.scatter(X[L_out][blues, 0], X[L_out][blues, 1], c="gold", s=20, marker='*', edgecolor='k')
+
+
     axs.set_aspect(aspect='equal')
-
     plt.show()
 
 
-n_samples = 1000
+
+
+N_SAMPLES = 1000
 outlier_ratio = 0.5
-X1, y1 = make_circles(n_samples=n_samples, factor=.6, noise=0.25)
+X1, y1 = make_circles(n_samples=N_SAMPLES, factor=.1, noise=0.1)
 y1[y1 == 0] = 1
 
 # X2, y2 = make_circles(n_samples=n_samples, factor=.6, noise=0.25)
@@ -182,46 +214,59 @@ xx, yy, XY = meshgrid(X)
 
 
 
-gram_Z = linear_kernel(XY, X_train)
-gram_z_diag = np.apply_along_axis(lambda xi : linear_kernel(xi.reshape(1, -1), xi.reshape(1, -1)), 1, XY)
+gram_Z = np.dot(XY, X_train.T)
+gram_z_diag = np.apply_along_axis(lambda xi : np.dot(xi, xi.T), 1, XY)
 
 
 # start with slighly too high initial outlier estimation
 # SVDD should learn decision boundary that is too tight, thus, excluding inliers
-nu = 0.5
-svdd = SVDD(kernel='linear', nu=nu, tol=10e-5, verbose=True)
+
+C = 1/(0.5*n_instances)
+svdd = SVDD(kernel='precomputed', C=C,verbose=True, tol=10e-6, shrinking=False)
 
 # initally sample weights are all 1
 W = np.ones(X_train.shape[0], dtype=np.float64)
 
 # conture plot for visualization
-svdd.fit(X_train, sample_weight=W)
-y_pred = svdd.predict(X_train)
-Z = svdd._decision_function(XY)
-Z = Z.reshape(xx.shape)
-decision_boundary(X_train, y_train, xx, yy, Z, 'Active learning decision boundary Train y_train')
-decision_boundary(X_train, y_pred, xx, yy, Z, 'Active learning decision boundary Train y_pred')
+svdd.fit(gram_train, W=W)
+y_pred_train = svdd.predict(gram_train, gram_diagonal_train)
+y_pred_test = svdd.predict(gram_test, gram_diagonal_test)
 
+y_pred_train_val = svdd.decision_function(gram_train, gram_diagonal_train)
+y_pred_test_val = svdd.decision_function(gram_test, gram_diagonal_test)
+
+plot_train_test_pred(X_train, X_test, y_train, y_test, y_pred_train, y_pred_test, np.sqrt(svdd.r_square))
+
+Z = svdd.decision_function(gram_Z, gram_z_diag)
+Z = Z.reshape(xx.shape)
+decision_boundary(X_train, y_train,xx, yy, Z, 'Decision boundary Train y_train')
+decision_boundary(X_train, y_pred_train, xx, yy, Z, 'Decision boundary Train y_pred')
+decision_boundary(X_test, y_test, xx, yy, Z, 'Decision boundary Test y_test')
+decision_boundary(X_test, y_pred_test, xx, yy, Z, 'Decision boundary Test y_pred')
 
 U = list(range(X_train.shape[0]))
 L_in = list()
 L_out = list()
 
-v_in = 1000.0
-v_out = 0.0001
+v_in = 10.0
+v_out = 0.01
 
 bal_accs = list()
 radii = list()
 # Active Learning Loop
 for i in range(50):
     print(f'Iteration: {i}')
-    #print(f'R^2 = {svdd.r_square}')
-    y_pred = svdd.predict(X_train)
-    y_vals = svdd._decision_function(X_train)
+    svdd = SVDD(kernel='precomputed', C=C, verbose=True, tol=10e-6, shrinking=False)
+    # train / retrain
+    svdd.fit(gram_train, W=W)
+    print(f'R = {np.sqrt(svdd.r_square)}')
+    radii.append(np.sqrt(svdd.r_square))
+    y_pred = svdd.predict(gram_train, gram_diagonal_train)
+    y_vals = svdd.decision_function(gram_train, gram_diagonal_train)
     bal_acc = balanced_accuracy_score(y_train, y_pred)
     bal_accs.append(bal_acc)
     # get most informative unknown sample outside decision boundary
-    idx_in_U = np.abs(np.where(y_vals[U] <= 0, y_vals[U], np.inf)).argmin()
+    idx_in_U = np.abs(np.where(y_vals[U] <= 0, np.inf, y_vals[U])).argmin()
     idx = U[idx_in_U]
     print(f'Most informative sample is {idx} with decision value {y_vals[idx]}')
     # ask oracle for annotation
@@ -237,29 +282,20 @@ for i in range(50):
         W[idx] = v_out
         L_out.append(idx)
     print(f'New sample weight W[{idx}]: {W[idx]}')
-    # retrain svdd
-    svdd.fit(X_train, sample_weight=W)
-    # radii.append(svdd.r_square)
     print('')
 
 plt.plot(list(range(50)), bal_accs)
+plt.plot(list(range(50)), radii)
 
-#plt.plot(list(range(50)), radii)
 
-gram_Z = linear_kernel(XY, X_train)
-gram_z_diag = np.apply_along_axis(lambda xi : linear_kernel(xi.reshape(1, -1), xi.reshape(1, -1)), 1, XY)
-
-Z = svdd._decision_function(XY)
+Z = svdd.decision_function(gram_Z, gram_z_diag)
 Z = Z.reshape(xx.shape)
 
-
-y_pred_train =svdd.predict(X_train)
-#y_pred_test = svdd.predict(gram_test, gram_diagonal_test)
-#y_pred_test = np.array(y_pred_test)
+y_pred_train = svdd.predict(gram_train, gram_diagonal_train)
 y_pred_train = np.array(y_pred_train)
 
-decision_boundary(X_train, y_train, xx, yy, Z, 'Active learning decision boundary Train y_train')
-decision_boundary(X_train, y_pred_train, xx, yy, Z, 'Active learning decision boundary Train y_pred')
+decision_boundary(X_train, y_train, xx, yy, Z, 'Active learning decision boundary Train y_train', (U, L_in, L_out))
+decision_boundary(X_train, y_pred_train, xx, yy, Z, 'Active learning decision boundary Train y_pred', (U, L_in, L_out))
 
 print("Annotated inliers:")
 print(L_in)
@@ -267,28 +303,5 @@ print("Annotated outliers:")
 print(L_out)
 
 
-#plot_train_test_pred(X_train, X_test, y_train, y_test, y_pred_train, y_pred_test)
-
 1
 
-
-# n_train = 30
-# length_train = 10
-# dim = 1
-#
-# rs = np.random.RandomState(1234)
-# c_train_matrix = rs.rand(n_train, length_train, dim).astype(dtype=np.float64, order='c')
-# res = np.zeros(shape=(n_train, n_train))
-# sigma = 2
-# triangular = 0
-# for i in range(n_train):
-#     seq_1 = c_train_matrix[i]
-#     for j in range(n_train):
-#         seq_2 = c_train_matrix[j]
-#         res[i, j] = tga_dissimilarity(seq_1, seq_2, sigma, triangular)
-# res = np.exp(-res)
-# res_cython = train_kernel_matrix(c_train_matrix, sigma, triangular, 'exp')
-# np.testing.assert_array_equal(res, res_cython)
-#
-# print(model.get_r2())
-# print(model.get_rho())
